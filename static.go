f9be04164bea835fe3109467a7d933dfd8b59297
firaga <jichen@mafengwo.cn>
2020-11-27 15:29:35 +0800

    网关服务

--- static.go
+++ static.go
@@ -4,7 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
-	//router.StaticFile("/a.png", "static/t.jpeg")
-	router.Static("s", "static")
+	router.StaticFile("/a.png", "static/t.jpeg")
+	//router.Static("s", "static")
+	router.StaticFS("s", gin.Dir("static", true))
 	router.Run(":8080")
 }
